dfc3697482f5af90d223ead9f6e04bd645cdd545
JohnFeng <fengguangwei.007@gmail.com>
2015-07-28 00:45:43 +0800

    create all fragments for archieve

---
+++ LockView/app/src/main/java/io/github/johnfeng/lockview/fragment/IntroImageFragment.java
@@ -0,0 +1,12 @@
+package io.github.johnfeng.lockview.fragment;
+
+/**
+ * Created by guangweifeng on 27/07/15.
+ */
+public class IntroImageFragment extends BaseFragment {
+
+    public static IntroImageFragment getInstance() {
+        IntroImageFragment fragment = new IntroImageFragment();
+        return fragment;
+    }
+}
---
+++ LockView/app/src/main/java/io/github/johnfeng/lockview/fragment/PreviewFragment.java
@@ -0,0 +1,11 @@
+package io.github.johnfeng.lockview.fragment;
+
+/**
+ * Created by guangweifeng on 27/07/15.
+ */
+public class PreviewFragment extends BaseFragment {
+    public static PreviewFragment getInstance() {
+        PreviewFragment fragment = new PreviewFragment();
+        return fragment;
+    }
+}
