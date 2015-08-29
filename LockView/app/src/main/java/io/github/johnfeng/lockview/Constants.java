79b8bd65fb220c43ff19d1bfde259cf956869cee
JohnFeng <fengguangwei.007@gmail.com>
2015-08-29 22:13:26 +0800

    IntroTopic Fragment

---
+++ LockView/app/src/main/java/io/github/johnfeng/lockview/Constants.java
@@ -0,0 +1,13 @@
+package io.github.johnfeng.lockview;
+
+/**
+ * Created by guangweifeng on 29/08/15.
+ */
+public class Constants {
+    public static final String KEY_FRAGMENT = "fragment";
+
+    public static final int EVENT_BASE = 10000;
+    public static final int EVENT_CHANGE_FRAGMENT = EVENT_BASE + 1;
+
+
+}
